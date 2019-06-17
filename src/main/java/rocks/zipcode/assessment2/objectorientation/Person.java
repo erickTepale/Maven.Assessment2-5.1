9cea665f06242d695aa341ec7687533ca5b2666a
Erick Tepale <e.tepale@gmail.com>
2019-06-17 13:36:03 -0400

    meh

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -5,40 +5,64 @@ package rocks.zipcode.assessment2.objectorientation;
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Address` class before attempting this class
  */
 public class Person {
+    private Long id;
+    private String name;
+    private Address address;
     /**
      * @param id - id of person
      * @param name - name of person
      * @param address - address of person
      */
     public Person(Long id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        this.id = Long.MIN_VALUE;
+        this.name = "";
+        this.address = new Address();
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public boolean equals(Person a) {
+
+
+        return (this.id.equals(a.id) &&
+                this.name.equals(a.name) &&
+                this.address.equals(a.address));
     }
 
     @Override
-    public boolean equals(Object o) {
-        return (Boolean)null;
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
     }
 }
